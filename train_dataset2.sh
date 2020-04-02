276b73741cbde1c0546cf6b5f4d8bdd4a7043d2f
Daniel Andreas <daniel-andreas@gmx.de>
2020-04-02 17:51:09 +0200

    bash scripts for automated benchmarks

---
+++ train_dataset2.sh
@@ -0,0 +1,46 @@
+cd Decoder 
+
+python3 Main.py 2 1 train &
+python3 Main.py 2 2 train &
+python3 Main.py 2 3 train &
+python3 Main.py 2 4 train &
+python3 Main.py 2 5 train &
+python3 Main.py 2 6 train &
+python3 Main.py 2 7 train &
+python3 Main.py 2 8 train
+
+python3 Main.py 2 9 train &
+python3 Main.py 2 10 train &
+python3 Main.py 2 11 train &
+python3 Main.py 2 12 train &
+python3 Main.py 2 13 train &
+python3 Main.py 2 14 train &
+python3 Main.py 2 15 train &
+python3 Main.py 2 16 train
+
+python3 Main.py 2 17 train &
+python3 Main.py 2 18 train &
+python3 Main.py 2 19 train &
+python3 Main.py 2 20 train &
+python3 Main.py 2 21 train &
+python3 Main.py 2 22 train &
+python3 Main.py 2 23 train &
+python3 Main.py 2 24 train
+
+python3 Main.py 2 25 train &
+python3 Main.py 2 26 train &
+python3 Main.py 2 27 train &
+python3 Main.py 2 28 train &
+python3 Main.py 2 29 train &
+python3 Main.py 2 30 train &
+python3 Main.py 2 31 train &
+python3 Main.py 2 32 train
+
+python3 Main.py 2 33 train &
+python3 Main.py 2 34 train &
+python3 Main.py 2 35 train &
+python3 Main.py 2 36 train &
+python3 Main.py 2 37 train &
+python3 Main.py 2 38 train &
+python3 Main.py 2 39 train &
+python3 Main.py 2 40 train
