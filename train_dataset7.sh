3e681d4a29e6da1ec1974e655f32ed031dce857d
Daniel Andreas <daniel-andreas@gmx.de>
2020-04-18 11:07:41 +0200

    benchmark scores completed

--- train_dataset7.sh
+++ train_dataset7.sh
@@ -1,13 +1,13 @@
 cd Decoder 
 
-python3 Main.py 7 1 train &
-python3 Main.py 7 2 train &
-python3 Main.py 7 3 train &
-python3 Main.py 7 4 train &
-python3 Main.py 7 5 train &
-python3 Main.py 7 6 train &
-python3 Main.py 7 7 train &
-python3 Main.py 7 8 train
+#python3 Main.py 7 1 train &
+#python3 Main.py 7 2 train &
+#python3 Main.py 7 3 train &
+#python3 Main.py 7 4 train &
+#python3 Main.py 7 5 train &
+#python3 Main.py 7 6 train &
+#python3 Main.py 7 7 train &
+#python3 Main.py 7 8 train
 
 python3 Main.py 7 9 train &
 python3 Main.py 7 10 train &
@@ -18,9 +18,9 @@ python3 Main.py 7 14 train &
 python3 Main.py 7 15 train &
 python3 Main.py 7 16 train
 
-python3 Main.py 7 17 train &
-python3 Main.py 7 18 train &
-python3 Main.py 7 19 train &
-python3 Main.py 7 20 train &
-python3 Main.py 7 21 train &
-python3 Main.py 7 22 train
+#python3 Main.py 7 17 train &
+#python3 Main.py 7 18 train &
+#python3 Main.py 7 19 train &
+#python3 Main.py 7 20 train &
+#python3 Main.py 7 21 train &
+#python3 Main.py 7 22 train
